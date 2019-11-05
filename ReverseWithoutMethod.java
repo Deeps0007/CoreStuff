f6b823a6f150928020fb114b1119344b421aae01
Deeps0007 <54866171+Deeps0007@users.noreply.github.com>
2019-11-05 14:48:26 +0530

    Without method

---
+++ ReverseWithoutMethod.java
@@ -0,0 +1,22 @@
+
+public class ReverseWithoutMethod {
+public static void main(String[] args) {
+	
+
+	String str="apple with orange";
+     
+	char []ch=str.toCharArray();
+
+	for(int i=ch.length-1;i>=0;i--)
+	{
+                         System.out.print(ch[i]);
+	}
+	
+	
+	
+	
+	
+	
+	
+}
+}
