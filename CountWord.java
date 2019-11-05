0e5630e6e9204e009c938b8e6990e07e62608ec9
Deeps0007 <54866171+Deeps0007@users.noreply.github.com>
2019-11-05 14:53:10 +0530

    Count word in a paragraph

---
+++ CountWord.java
@@ -0,0 +1,26 @@
+
+public class CountWord {
+
+	
+	
+	public static void main(String[] args) {
+		
+		
+		String str="One by One have two same word frequency";
+		
+		String []arr=str.split(" ");
+		
+		System.out.println(arr.length);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+}
